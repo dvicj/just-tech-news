162f8654f935f45c6feec50c08d3b49d1bf1a97d
dvicjon <devinvjones@gmail.com>
2021-03-03 17:17:44 -0500

    13.3.5

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+//13.3.5 - create associations
+User.hasMany(Post, { //one to many
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, { //many to one
+    foreignKey: 'user_id',
+});
+
+
 module.exports = { User, Post };
