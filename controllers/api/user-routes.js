a53727cadca01d8ca2dfdd6bcda4d37416661e47
dvicjon <devinvjones@gmail.com>
2021-03-07 21:53:56 -0500

    14.2.5

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,32 +63,44 @@ router.post('/', (req,res) => {
         email: req.body.email,
         password: req.body.email,
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-});
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+      
+            res.json(dbUserData);
+        });
+    })
+}); 
 
 //13.2.6 
 router.post('/login', (req, res) => {
     User.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(400).json({ message: 'No user with that email address!' });
-            return; 
-        }
-        //13.2.6 - verify user 
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        if(!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!' });
-            return; 
-        }
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
         res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
 });
 
--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false, 
+  saveUninitialized: true, 
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 //14.1.4 - set up handlebars as app's templat engine of choice 
 const exphbs = require('express-handlebars');
@@ -15,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
