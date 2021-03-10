ad1b1d1f17f5145d247b467427ba4c452ff24c06
dvicjon <devinvjones@gmail.com>
2021-03-10 18:06:57 -0500

    14.4.6

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,14 @@
 const {format_date} = require('../utils/helpers');
+const {format_plural} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
   const date = new Date('2020-03-20 16:12:03');
 
   expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() pluralizes a word', () => {
+    const word = "tiger",2
+
+    expect(format_plural(word)).toBe('tigers');
 });
